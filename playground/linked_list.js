fc6a5280f11a8335f5d3a5f61e8b84e3845054a2
David Rio Deiros <driodeiros@gmail.com>
2013-09-15 14:51:04 -0400

    Adding remove elements to linked list.

--- playground/linked_list.js
+++ playground/linked_list.js
@@ -14,13 +14,51 @@ function create(spec) {
     return (ll.first == ll.last && ll.first === null && ll.last === null);
   };
 
-  ll.push = function(e) {
+  ll.add = function(e) {
     var new_element = { data: e, next: null };
     if (ll.empty())
       ll.first = new_element;
     else
       ll.last.next = new_element;
     ll.last = new_element;
+
+    return ll;
+  };
+
+  // Time Cost O(n)
+  // returns the number of elements removed
+  ll.rm = function(e_to_rm) {
+    var n_del = 0,
+        curr,
+        prev;
+
+    if (ll.empty())
+      return 0;
+
+    if (ll.first === ll.last && ll.first.data === e_to_rm) {
+      ll.first = null;
+      ll.last = null;
+      return 1;
+    }
+
+    // common case
+    curr = ll.first;
+    prev = null;
+    while (curr !== null) {
+      if (curr.data === e_to_rm) {
+        n_del += 1;
+        prev.next = curr.next;
+        curr = curr.next;
+        if (curr === null)
+          ll.last = prev;
+      }
+      else {
+        prev = curr;
+        curr = curr.next;
+      }
+    }
+
+    return n_del;
   };
 
   return ll;
