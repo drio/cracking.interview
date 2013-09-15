d1af044430455293886c18049f363b9a78d75bae
David Rio Deiros <driodeiros@gmail.com>
2013-09-15 19:12:24 -0400

    Cleaner interface that gives the user more control on the list.

--- playground/linked_list.js
+++ playground/linked_list.js
@@ -3,62 +3,41 @@ exports.create = create;
 function create(spec) {
   var ll = function() {};
 
-  ll.talk = function() {
-    console.log("Yes: " + spec);
-  };
-
-  ll.first = null;
-  ll.last  = null;
+  ll.first   = null;
+  ll.current = null;
 
   ll.empty = function() {
-    return (ll.first == ll.last && ll.first === null && ll.last === null);
+    return (ll.first == ll.last && ll.first === null);
   };
 
+  // Move the current pointer to the next element in the list
+  ll.next = function() {
+    if (ll.empty())
+      return null;
+    else
+      if (ll.current) ll.current = ll.current.next;
+
+    return ll.current;
+  };
+
+  // Add e right after current
   ll.add = function(e) {
     var new_element = { data: e, next: null };
-    if (ll.empty())
+
+    if (ll.empty()) {
       ll.first = new_element;
-    else
-      ll.last.next = new_element;
-    ll.last = new_element;
+      ll.current = { next: ll.first};
+    }
+    else if (!ll.empty() && ll.current === null) {
+      new_element.next = ll.first.next;
+      ll.first = new_element;
+    }
+    else {
+      new_element.next = ll.current.next;
+      ll.current.next  = new_element;
+    }
 
     return ll;
-  };
-
-  // Time Cost O(n)
-  // returns the number of elements removed
-  ll.rm = function(e_to_rm) {
-    var n_del = 0,
-        curr,
-        prev;
-
-    if (ll.empty())
-      return 0;
-
-    if (ll.first === ll.last && ll.first.data === e_to_rm) {
-      ll.first = null;
-      ll.last = null;
-      return 1;
-    }
-
-    // common case
-    curr = ll.first;
-    prev = null;
-    while (curr !== null) {
-      if (curr.data === e_to_rm) {
-        n_del += 1;
-        prev.next = curr.next;
-        curr = curr.next;
-        if (curr === null)
-          ll.last = prev;
-      }
-      else {
-        prev = curr;
-        curr = curr.next;
-      }
-    }
-
-    return n_del;
   };
 
   return ll;
--- playground/test_linked_list.js
+++ playground/test_linked_list.js
@@ -5,37 +5,10 @@ var assert = require('assert'),
 
 ml = ll.create();
 assert(ml.empty());
-
 ml.add(1);
-assert(!ml.empty());
-
-assert(ml.first.data === 1);
-assert(ml.last.data === 1);
-
 ml.add(2);
-assert(ml.first.data === 1);
-assert(ml.last.data === 2);
 
-ml.add(3); // 1,2,3
-
-assert(ml.rm(2) === 1);
-assert(ml.first.data === 1);
-assert(ml.last.data === 3);
-
-assert(ml.rm(3) === 1);
-assert(ml.last.data === 1);
-assert(ml.first.data === 1);
-
-assert(ml.rm(12) === 0);
-
-assert(ml.rm(1) === 1);
-assert(ml.rm(12) === 0);
-
-ml.add(1).add(2).add(2);
-assert(ml.rm(2) === 2);
-
-assert(ml.first.data === 1);
-assert(ml.last.data === 1);
-ml.rm(1);
-assert(ml.empty());
-
+assert(ml.next().data === 2);
+assert(ml.next().data === 1);
+assert(ml.next() === null);
+assert(ml.next() === null);
