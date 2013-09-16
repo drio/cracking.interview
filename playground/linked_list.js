0b8ed5207d495d04004f648dd7613acc7b8fe7da
David Rio Deiros <driodeiros@gmail.com>
2013-09-15 23:24:31 -0400

    Fixing bugs in previous implementation.

--- playground/linked_list.js
+++ playground/linked_list.js
@@ -3,26 +3,41 @@ exports.create = create;
 function create(spec) {
   var ll = function() {};
 
-  ll.first   = null;
-  ll.current = null;
+  var first, last, current, end_reached;
+
+  function init() {
+    first       = null;
+    last        = null;
+    current     = null;
+    end_reached = false;
+  }
+
+  init();
 
   ll.rewind = function() {
-    ll.current = { next: ll.first};
+    current = null;
+    end_reached = false;
     return ll;
   };
 
   ll.empty = function() {
-    return (ll.first === null);
+    return (first === null);
   };
 
   // Move the current pointer to the next element in the list
   ll.next = function() {
     if (ll.empty())
       return null;
-    else
-      if (ll.current) ll.current = ll.current.next;
+    else if (!current && end_reached) // At the end and asking to move
+      return null;
+    else if (!current && !end_reached)
+      current = first;
+    else {
+      current = current.next;
+      if (!current) end_reached = true;
+    }
 
-    return ll.current;
+    return current ? current.data : null;
   };
 
   // Add e right after current
@@ -30,18 +45,44 @@ function create(spec) {
     var new_element = { data: e, next: null };
 
     if (ll.empty()) {
-      ll.first = new_element;
-      ll.current = { next: new_element};
+      first = new_element;
+      last  = new_element;
     }
     else {
-      new_element.next = ll.current.next;
-      ll.current.next  = new_element;
+      if (current) {
+        new_element.next = current.next;
+        current.next  = new_element;
+        if (current === last)
+          last = new_element;
+      }
+      else { // current not set, insert at the beginning
+        new_element.next = first;
+        first = new_element;
+      }
     }
 
     return ll;
   };
 
   ll.rm = function() {
+    var p, e;
+    if (ll.empty()) return null;
+    if (!current) return null;
+    if (first === current) {
+      e = first.data;
+      init();
+    }
+    else {
+      p = { next:first };
+      while (p.next !== current)
+        p = p.next;
+      if (p.next === last)
+        last = p;
+      e = p.next.data;
+      p.next = p.next.next;
+    }
+
+    return e;
   };
 
   return ll;
--- playground/test_linked_list.js
+++ playground/test_linked_list.js
@@ -6,21 +6,27 @@ var assert = require('assert'),
 // Basic create/add/next
 ml = ll.create();
 assert(ml.empty());
-ml.add(1);
-ml.add(2);
 
-assert(ml.next().data === 2);
-assert(ml.next().data === 1);
+assert(!ml.next());
+ml.add(1);
+assert(!ml.empty());
+assert(ml.next() === 1);
+ml.add(2);
+assert(ml.next() === 2);
+assert(!ml.next());
 assert(!ml.next());
 
-// More create/add/next but now playing with next to insert in order
 ml = ll.create();
-assert(ml.empty());
+assert(!ml.rm());
 ml.add(1);
+assert(!ml.rm());
 ml.next();
-ml.add(2);
+assert(ml.rm() === 1);
+assert(ml.empty());
+ml.add(1).add(2).add(3);
+assert(ml.next() === 3);
+assert(ml.next() === 2);
+assert(ml.rm() === 2);
+assert(ml.next() === 1);
 
-ml.rewind();
-assert(ml.next().data === 1);
-assert(ml.next().data === 2);
-assert(!ml.next());
+
